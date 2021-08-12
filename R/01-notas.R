29e584b5978ffde29a462d99ff3c866dcad070ef
Pablo Yamild Rosiles Loeza <pyrosilesl97@gmail.com>
2021-08-12 13:42:01 -0500

    b

--- R/01-notas.R
+++ R/01-notas.R
@@ -16,3 +16,33 @@
 
 
 #Uso del booleano %in% int_gen
+
+
+library("gitcreds", "gert")
+library('gh')
+
+#Revisar la configuración de Git
+gh::gh_whoami() # para checar cómo quedó la configuración
+
+gert::git_commit_all("Nuevos")
+gert::git_push()
+
+usethis::use_r('08_reduccion_de_dimensiones.Rmd')
+
+##Change the error
+#options(error= function() starwarssay::say("Do or do not... There is no try", by = "baby_yoda"))
+
+options(prompt = "🦖 ~ 🧬 >")
+
+starwarssay::say("Join the dark side.... #CODE \n Welcome back Pablo", by = "darth_vader")
+
+usethis::edit_r_profile()
+if (interactive()) {
+  .First <- function() {
+    # aquí van los mensajes o las funciones para la bienvenida
+  }
+
+  .Last <- function() {
+    # aquí van los mensajes o las funciones para la despedida
+  }
+}
